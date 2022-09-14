0576f90d0f8035f80517dec7974a6668d3917682
nicwu25 <nicwu25@gmail.com>
2022-09-14 09:25:03 +0800

    Initial Commit

---
+++ SwiftUIxRedux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIxRedux
+//
+//  Created by Nic Wu on 2022/9/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIxRedux/SwiftUIxReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIxReduxApp.swift
+//  SwiftUIxRedux
+//
+//  Created by Nic Wu on 2022/9/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIxReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
